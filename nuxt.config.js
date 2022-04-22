157ebbfde377fc18f2ca67dc6ca5d1ebb758a124
byte-wolf <s.wolffhardt@protonmail.com>
2022-04-22 10:11:18 +0200

    added favicon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'invisible-wall',
+    title: 'Glasdesign Wolffhardt',
     htmlAttrs: {
       lang: 'en',
     },
