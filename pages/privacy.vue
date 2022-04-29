96d4417e0e3fd632e6537aec9280229107e124ef
byte-wolf <s.wolffhardt@protonmail.com>
2022-04-29 17:00:09 +0200

    added privacy

---
+++ pages/privacy.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="py-32">
+        <div class="bg-white rounded-md p-4 container mx-auto shadow-lg">
+            <div class="w-96">
+                <h1 class="font-bold text-xl mb-4">
+                    Datenschutzerklärung
+                </h1>
+
+                <h2 class="font-bold mb-1">
+                    Vorwort
+                </h2>
+                <p>
+                    
+                    Der Schutz Ihrer persönlichen Daten ist uns ein besonderes Anliegen.
+                    Wir verarbeiten Ihre Daten daher ausschließlich auf Grundlage der
+                    gesetzlichen Bestimmungen (DSGVO, TKG 2003). In diesen
+                    Datenschutzinformationen informieren wir Sie über die wichtigsten
+                    Aspekte der Datenverarbeitung im Rahmen unserer Website.
+                </p>
+
+                <h2 class="font-bold mb-1 mt-5">
+                    Kontakt mit uns
+                </h2>
+                <p>
+                    Wenn Sie per Formular auf der Website oder per E-Mail Kontakt mit
+                    uns aufnehmen, werden Ihre angegebenen Daten zwecks Bearbeitung der
+                    Anfrage und für den Fall von Anschlussfragen bei uns gespeichert.
+                    Diese Daten geben wir nicht ohne Ihre Einwilligung weiter.
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style>
+
+</style>
